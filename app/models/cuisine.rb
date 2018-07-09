b4699d601306f048689581ece0729bfdfeb92ec8
aviv.oren <aviv.oren@wework.com>
2018-07-09 17:33:00 +0300

    validates

--- app/models/cuisine.rb
+++ app/models/cuisine.rb
@@ -1,2 +1,4 @@
 class Cuisine < ApplicationRecord
+  validates_presence_of :name # Validates that the specified attributes are not blank
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,6 @@
 class Restaurant < ApplicationRecord
 
-  validates_presence_of :name, :address, :max_delivery_time_minutes
-  validates_inclusion_of :accepts_10bis, in: [true, false]
+  validates_presence_of :name, :address, :max_delivery_time_minutes # Validates that the specified attributes are not blank
+  validates_inclusion_of :accepts_10bis, in: [true, false] #
 
 end
