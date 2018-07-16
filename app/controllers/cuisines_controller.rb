a43b9e587cc8aed1f15bcc303b2ee3fc2bd1c260
aviv.oren <aviv.oren@wework.com>
2018-07-16 11:57:30 +0300

    controller cleanup l

--- app/controllers/cuisines_controller.rb
+++ app/controllers/cuisines_controller.rb
@@ -5,70 +5,31 @@ class CuisinesController < ApplicationController
   # GET /cuisines.json
   def index
     @cuisines = Cuisine.all
-  end
-
-  # GET /cuisines/1
-  # GET /cuisines/1.json
-  def show
-  end
-
-  # GET /cuisines/new
-  def new
-    @cuisine = Cuisine.new
-  end
-
-  # GET /cuisines/1/edit
-  def edit
+    render :json => @cuisines
   end
 
   # POST /cuisines
   # POST /cuisines.json
   def create
     @cuisine = Cuisine.new(cuisine_params)
-
-    respond_to do |format|
-      if @cuisine.save
-        format.html { redirect_to @cuisine, notice: 'Cuisine was successfully created.' }
-        format.json { render :show, status: :created, location: @cuisine }
-      else
-        format.html { render :new }
-        format.json { render json: @cuisine.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /cuisines/1
-  # PATCH/PUT /cuisines/1.json
-  def update
-    respond_to do |format|
-      if @cuisine.update(cuisine_params)
-        format.html { redirect_to @cuisine, notice: 'Cuisine was successfully updated.' }
-        format.json { render :show, status: :ok, location: @cuisine }
-      else
-        format.html { render :edit }
-        format.json { render json: @cuisine.errors, status: :unprocessable_entity }
-      end
-    end
+    render json: @cuisine, status: :created
   end
 
   # DELETE /cuisines/1
   # DELETE /cuisines/1.json
   def destroy
     @cuisine.destroy
-    respond_to do |format|
-      format.html { redirect_to cuisines_url, notice: 'Cuisine was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    format.json { head :no_content }
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_cuisine
-      @cuisine = Cuisine.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_cuisine
+    @cuisine = Cuisine.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def cuisine_params
-      params.require(:cuisine).permit(:name)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def cuisine_params
+    params.require(:cuisine).permit(:name)
+  end
 end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -14,15 +14,6 @@ class RestaurantsController < ApplicationController
      render :json => @restaurant
   end
 
-  # # GET /restaurants/new
-   def new
-     @restaurant = Restaurant.new
-   @cuisine = @restaurant.combo
-   end
-
-   # GET /restaurants/1/edit
-   def edit
-   end
 
   # POST /restaurants
   # POST /restaurants.json
@@ -31,35 +22,19 @@ class RestaurantsController < ApplicationController
 
     respond_to do |format|
       if @restaurant.save
-        format.html { redirect_to @restaurant, notice: 'Restaurant was successfully created.' }
         format.json { render :show, status: :created, location: @restaurant }
       else
-        format.html { render :new }
         format.json { render json: @restaurant.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /restaurants/1
-  # PATCH/PUT /restaurants/1.json
-  def update
-    respond_to do |format|
-      if @restaurant.update(restaurant_params)
-        format.html { redirect_to @restaurant, notice: 'Restaurant was successfully updated.' }
-        format.json { render :show, status: :ok, location: @restaurant }
-      else
-        format.html { render :edit }
-        format.json { render json: @restaurant.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 
   # DELETE /restaurants/1
   # DELETE /restaurants/1.json
   def destroy
     @restaurant.destroy
     respond_to do |format|
-      format.html { redirect_to restaurants_url, notice: 'Restaurant was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -74,4 +49,5 @@ class RestaurantsController < ApplicationController
     def restaurant_params
       params.require(:restaurant).permit(:name, :cuisine_id, :rating, :accepts_10bis, :address, :max_delivery_time_minutes)
     end
+
 end
